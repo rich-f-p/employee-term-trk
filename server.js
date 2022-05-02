3da89a26220d1004a08de1a3cb9443bfb0504978
rich p <richardferry3@gmail.com>
2022-05-02 12:32:56 -0700

    add function for displaying all of the departments that are currently in the database

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const ct = require('console.table');
 const inquire = require('inquirer');
 const mySql = require('mysql2');
 
+// function for console.log
+function cl(log){
+    console.log(log);
+};
+
 const my = mySql.createConnection(
     {
         host: 'localhost',
@@ -9,4 +14,18 @@ const my = mySql.createConnection(
         password: 'password123',
         database: 'emply_db'
     }
-)
+);
+
+//function that will return all saved departments in the database
+function getDepartment(){
+    const department = `SELECT * FROM department;`
+    my.query(department,
+        function(err,results,fields){
+            if(err){
+                cl(err);              
+            }
+            console.table(results);
+        }
+    )
+};
+
