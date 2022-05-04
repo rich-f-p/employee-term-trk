85ed2fb2bf8bf70fcd5d088d54b4bcb02b88c17c
rich p <richardferry3@gmail.com>
2022-05-04 00:35:05 -0700

    add another manager employee

--- db/seeds.sql
+++ db/seeds.sql
@@ -11,5 +11,6 @@ VALUES ('engineer', 100, 4),
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ('bill', 'wilbert', 1, NULL),
+        ('tim','scott',2,NULL),
        ('john', 'steven', 2, 1);
 
